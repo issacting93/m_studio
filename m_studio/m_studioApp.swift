228b0fbe260860edab2f18411e7a164441ffe05f
issacting93 <issacting38@gmail.com>
2026-05-16 17:22:00 -0400

    Initial Commit

---
+++ m_studio/m_studioApp.swift
@@ -0,0 +1,17 @@
+//
+//  m_studioApp.swift
+//  m_studio
+//
+//  Created by Issac Ting on 16/5/26.
+//
+
+import SwiftUI
+
+@main
+struct m_studioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
